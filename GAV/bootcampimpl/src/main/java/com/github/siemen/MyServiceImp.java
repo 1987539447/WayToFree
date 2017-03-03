90e69e79087eb781573e00f664f3f1cd894e3b3a
Zhan <1987539447@qq.com>
2017-03-03 17:08:52 +0800

    多模块工程

---
+++ GAV/bootcampimpl/src/main/java/com/github/siemen/MyServiceImp.java
@@ -0,0 +1,10 @@
+package com.github.siemen;
+
+/**
+ * Created by Zhan on 2017/3/3 0003.
+ */
+public class MyServiceImp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ GAV/bootcapapi/src/main/java/MyService.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Zhan on 2017/3/3 0003.
+ */
+public interface MyService {
+}
